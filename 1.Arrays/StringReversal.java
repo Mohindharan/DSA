ded17d4d7cc156b57d56b0c779cd8a5652150feb
mohindharan <mohindharan13@gmail.com>
2023-07-07 15:53:57 +0530

    DSA array 1

---
+++ 1.Arrays/StringReversal.java
@@ -0,0 +1,54 @@
+// Given an array (or string), the task is to reverse the array/string.
+
+// **Examples :**
+// Input  : arr[] = {1, 2, 3}
+// Output : arr[] = {3, 2, 1}
+
+// Input :  arr[] = {4, 5, 1, 2}
+// Output : arr[] = {2, 1, 5, 4}
+
+
+// Iterative java program to reverse an
+// array
+public class StringReversal {
+	
+/* Function to reverse */
+	static void rvereseArray(int arr[])
+	{
+		int temp;
+        int start =0;
+		int end= arr.length-1;
+        
+		while (start < end)
+		{
+			temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}	
+	
+	/* Utility that prints out an
+	array on a line */
+	static void printArray(int arr[],
+							int size)
+	{
+		for (int i = 0; i < size; i++)
+			System.out.print(arr[i] + " ");
+		
+		System.out.println();
+	}
+
+	// Driver code
+	public static void main(String args[]) {
+		
+		int arr[] = {1, 2, 3, 4, 5, 6};
+		printArray(arr, 6);
+		rvereseArray(arr);
+		System.out.print("Reversed array is \n");
+		printArray(arr, 6);
+		
+	}
+}
+
